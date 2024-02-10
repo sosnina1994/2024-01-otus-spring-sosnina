f9cca6f8cb4ca81c0e367ab821bfc3d101bcde7a
SosninaNV <n.sosnina1994@yandex.ru>
2024-02-10 14:14:12 +0500

    fix mistake in AnswerCsvConverter

--- hw01-xml-config/src/main/java/ru/otus/hw/dao/dto/AnswerCsvConverter.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/dao/dto/AnswerCsvConverter.java
@@ -7,7 +7,7 @@ public class AnswerCsvConverter extends AbstractCsvConverter {
 
     @Override
     public Object convertToRead(String value) {
-        var valueArr = "%".split(value);
+        var valueArr = value.split("%");
         return new Answer(valueArr[0], Boolean.parseBoolean(valueArr[1]));
     }
 }
