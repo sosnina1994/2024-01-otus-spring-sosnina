a2f8e7bd15f92be37543d091aa3ebf96ce9e2050
SosninaNV <n.sosnina1994@yandex.ru>
2024-02-10 14:37:27 +0500

    delete the checks

--- hw01-xml-config/src/main/java/ru/otus/hw/service/QuestionStringFormatter.java
+++ hw01-xml-config/src/main/java/ru/otus/hw/service/QuestionStringFormatter.java
@@ -5,20 +5,9 @@ import ru.otus.hw.domain.Question;
 
 public class QuestionStringFormatter {
     public static String formatQuestion(Question question) {
-        if (question == null || question.text().trim().isEmpty()) {
-            return null;
-        }
-
         String res = String.format("%s: \n", question.text());
-        if (question.answers() == null || question.answers().isEmpty()) {
-            return res;
-        }
-
         int num = 1;
         for (Answer answer : question.answers()) {
-            if (answer.text() == null || answer.text().trim().isEmpty()) {
-                continue;
-            }
             res = res.concat(String.format("%d %s\t", num++, answer.text()));
         }
         return res.concat("\n");
