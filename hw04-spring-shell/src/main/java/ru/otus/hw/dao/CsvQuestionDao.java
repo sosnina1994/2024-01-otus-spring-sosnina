afe2a81abee5829de9ef1976089e09e05f45cd63
SosninaNV <n.sosnina1994@yandex.ru>
2024-03-03 10:02:19 +0500

    fix mistake

--- hw04-spring-shell/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw04-spring-shell/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -25,7 +25,8 @@ public class CsvQuestionDao implements QuestionDao {
     @Override
     public List<Question> findAll() {
         ClassLoader loader = getClass().getClassLoader();
-        try (InputStream is = Objects.requireNonNull(loader.getResourceAsStream(fileNameProvider.getTestFileNameByLocaleTag()))) {
+        try (InputStream is = Objects.requireNonNull(
+                loader.getResourceAsStream(fileNameProvider.getTestFileNameByLocaleTag()))) {
 
             MappingStrategy<QuestionDto> strategy = new ColumnPositionMappingStrategyBuilder<QuestionDto>().build();
             strategy.setType(QuestionDto.class);
@@ -43,7 +44,6 @@ public class CsvQuestionDao implements QuestionDao {
                 }
             }
             return result;
-
         } catch (Exception ex) {
             throw new QuestionReadException("Непредвиденная ошибка при чтении файла", ex);
         }
