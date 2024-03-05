717d1ff22df5137c86b1b106c2da347e08ed0c25
SosninaNV <n.sosnina1994@yandex.ru>
2024-03-05 19:57:26 +0500

    delete ApplicationConfig

--- hw04-spring-shell/src/main/java/ru/otus/hw/Hw04SpringShellApplication.java
+++ hw04-spring-shell/src/main/java/ru/otus/hw/Hw04SpringShellApplication.java
@@ -2,8 +2,11 @@ package ru.otus.hw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.otus.hw.config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class Hw04SpringShellApplication {
 
 	public static void main(String[] args) {
