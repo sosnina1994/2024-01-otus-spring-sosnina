3656ae78e46453a954766256502d274eb17bdd7c
SosninaNV <n.sosnina1994@yandex.ru>
2024-04-20 02:13:23 +0500

    add hw10 for angular

---
+++ hw10-spring-web-page/backend/src/main/java/ru/otus/hw/models/Author.java
@@ -0,0 +1,26 @@
+package ru.otus.hw.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+}
