3835530eb1f3e26e973658b8c29ffb80680522da
SosninaNV <n.sosnina1994@yandex.ru>
2024-04-26 06:47:49 +0500

    fix hw09

--- hw10-spring-web-page/backend/src/main/java/ru/otus/hw/controllers/ExceptionController.java
+++ hw10-spring-web-page/backend/src/main/java/ru/otus/hw/controllers/ExceptionController.java
@@ -1,28 +1,42 @@
 package ru.otus.hw.controllers;
 
+import lombok.extern.slf4j.*;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import ru.otus.hw.exceptions.NotFoundException;
 
 import java.util.List;
+import java.util.Arrays;
 
-@ControllerAdvice
+@Slf4j
+@RestControllerAdvice
 public class ExceptionController {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleEntityNotFoundEx(NotFoundException ex) {
-        return ResponseEntity.notFound().build();
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
+    public String handleEntityNotFoundEx(NotFoundException ex) {
+        log.warn(ex.getMessage());
+        return ex.getMessage();
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<List<String>> handleEntityValidateEx(MethodArgumentNotValidException ex) {
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public List<String> handleEntityValidateEx(MethodArgumentNotValidException ex) {
         final String[] errors = ex.getAllErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage).toArray(String[]::new);
-        return ResponseEntity.badRequest()
-                .header("errors", errors)
-                .build();
+        log.warn(Arrays.toString(errors));
+        return Arrays.stream(errors).toList();
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public String handleRuntimeException(RuntimeException ex) {
+        String error = "Internal error occured, " + ex.getStackTrace()[0];
+        log.error(error);
+        return error;
     }
 }
