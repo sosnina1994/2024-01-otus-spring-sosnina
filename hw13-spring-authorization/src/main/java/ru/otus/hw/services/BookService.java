be9e2acd09df85b5bf56c67149ecb9fa51df233b
SosninaNV <n.sosnina1994@yandex.ru>
2024-05-26 22:55:29 +0500

    fix hw 13

--- hw13-spring-authorization/src/main/java/ru/otus/hw/services/BookService.java
+++ hw13-spring-authorization/src/main/java/ru/otus/hw/services/BookService.java
@@ -1,6 +1,5 @@
 package ru.otus.hw.services;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import ru.otus.hw.dto.BookCreateDto;
 import ru.otus.hw.dto.BookDto;
 import ru.otus.hw.dto.BookUpdateDto;
@@ -8,18 +7,13 @@ import ru.otus.hw.dto.BookUpdateDto;
 import java.util.List;
 
 public interface BookService {
-    @PreAuthorize("hasRole('USER')")
     BookDto findById(Long id);
 
-    @PreAuthorize("hasRole('USER')")
     List<BookDto> findAll();
 
-    @PreAuthorize("hasRole('USER')")
     BookDto create(BookCreateDto bookDto);
 
-    @PreAuthorize("hasRole('ADMIN')")
     BookDto update(BookUpdateDto bookDto);
 
-    @PreAuthorize("hasRole('ADMIN')")
     void deleteById(Long id);
 }
--- hw13-spring-authorization/src/main/java/ru/otus/hw/services/UserService.java
+++ hw13-spring-authorization/src/main/java/ru/otus/hw/services/UserService.java
@@ -1,17 +1,13 @@
 package ru.otus.hw.services;
 
-import org.springframework.security.access.prepost.PreAuthorize;
 import ru.otus.hw.dto.UserDto;
 
 import java.util.List;
 
 public interface UserService {
-    @PreAuthorize("hasRole('ADMIN')")
     List<UserDto> findAll();
 
-    @PreAuthorize("hasRole('ADMIN')")
     UserDto findByUsername(String username);
 
-    @PreAuthorize("hasRole('ADMIN')")
     void deleteUser(UserDto dto);
 }
