2ef90fe8926fbb8563d7e61fd89b97714c1a796d
SosninaNV <n.sosnina1994@yandex.ru>
2024-07-18 02:33:04 +0500

    fix toolTypes

--- final-project/warehouse-management-service/src/main/java/ru/otus/hw/controllers/ToolTypeController.java
+++ final-project/warehouse-management-service/src/main/java/ru/otus/hw/controllers/ToolTypeController.java
@@ -2,8 +2,12 @@ package ru.otus.hw.controllers;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestBody;
 import ru.otus.hw.dto.ToolTypeDto;
 import ru.otus.hw.services.ToolTypeService;
 
