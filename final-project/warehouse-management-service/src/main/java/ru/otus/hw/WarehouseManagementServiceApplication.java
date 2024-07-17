bc290f58f5b4d213c22cde9bf53d376199833fa6
SosninaNV <n.sosnina1994@yandex.ru>
2024-07-17 22:33:29 +0500

    add preparation of the final project

---
+++ final-project/warehouse-management-service/src/main/java/ru/otus/hw/WarehouseManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.otus.hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseManagementServiceApplication.class, args);
+	}
+
+}
