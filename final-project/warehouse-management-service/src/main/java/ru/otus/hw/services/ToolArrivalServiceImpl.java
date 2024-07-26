71eb547a61bbed7780718d3e3b363e722d772845
SosninaNV <n.sosnina1994@yandex.ru>
2024-07-26 20:10:22 +0500

    add ToolArrivalService

---
+++ final-project/warehouse-management-service/src/main/java/ru/otus/hw/services/ToolArrivalServiceImpl.java
@@ -0,0 +1,52 @@
+package ru.otus.hw.services;
+
+import lombok.val;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.otus.hw.dto.ToolArrivalDto;
+import ru.otus.hw.dto.ToolArrivalCreateDto;
+import ru.otus.hw.exceptions.NotFoundException;
+import ru.otus.hw.mappers.ToolArrivalMapper;
+import ru.otus.hw.models.ToolArrivalAct;
+import ru.otus.hw.models.ToolBalance;
+import ru.otus.hw.repositories.ToolBalanceRepository;
+import ru.otus.hw.repositories.ToolArrivalRepository;
+
+@Service
+@RequiredArgsConstructor
+public class ToolArrivalServiceImpl implements ToolArrivalService {
+
+    private final ToolBalanceRepository toolBalanceRepository;
+    private final ToolArrivalRepository toolArrivalRepository;
+
+    private final ToolArrivalMapper toolArrivalMapper;
+
+    @Override
+    @Transactional
+    public ToolArrivalDto create(ToolArrivalCreateDto toolArrivalCreateDto) {
+        val toolBalance = toolBalanceRepository
+                .findToolBalancesByToolId(toolArrivalCreateDto.getToolId())
+                .orElseThrow(() ->
+                        new NotFoundException("Tool balance with id = %d is not found"
+                                .formatted(toolArrivalCreateDto.getToolId())));
+
+        val changingBalance = toolBalance.getCurrentBalance() + toolArrivalCreateDto.getCount();
+
+        toolBalance.setCurrentBalance(changingBalance);
+        toolBalanceRepository.save(toolBalance);
+
+        val act = mapToAct(toolArrivalCreateDto, toolBalance);
+
+        val toolArrivalAct = toolArrivalRepository.save(act);
+        return toolArrivalMapper.mapToDto(toolArrivalAct);
+    }
+
+    private ToolArrivalAct mapToAct(ToolArrivalCreateDto toolArrivalCreateDto, ToolBalance balance) {
+        val act = new ToolArrivalAct();
+        act.setTool(balance.getTool());
+        act.setCount(toolArrivalCreateDto.getCount());
+        act.setOrderNumber(toolArrivalCreateDto.getOrderNumber());
+        return act;
+    }
+}
